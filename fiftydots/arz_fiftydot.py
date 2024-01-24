f3163bc732c97b631bc1f194318f4b7f404489ee
arz <111977867+arzshz@users.noreply.github.com>
2024-01-24 16:25:07 +0330

    arz_fiftydot.py

--- fiftydots/arz_fiftydot.py
+++ fiftydots/arz_fiftydot.py
@@ -1,52 +1,42 @@
 import random
 import turtle
-import time
-from math import sqrt
 
 myPen = turtle.Turtle()
 screen = turtle.Screen()
 screen.setup(800, 800)
-myPen.speed(10)
+myPen.speed(50)
 myPen.color("black")
-myPen.pensize(3)
+myPen.pensize(2)
+screen.tracer(0)
+myPen.setheading(0)
 
-myPen.penup()
-myPen.goto(0, 0)
-myPen.pendown()
+for i in range(50): 
+    myPen.penup()
+    myPen.goto(0, 0)
+    myPen.pendown()
 
-def distance(list):
-    radious = sqrt(list[0]**2 + list[1]**2)
-    return radious
-
-final_list = []
-
-for i in range(49):
-    x = (random.random()*400)*((-1)**random.randint(1,10))
-    y = (random.random()*400)*((-1)**random.randint(1,10))
-    final_list.append([x, y])
-
-final_list = sorted(final_list, key= distance)
-
-last_dot_num = random.randrange(-401,401)
-
-final_dot_list = [[-400, last_dot_num], [last_dot_num, -400], [400, last_dot_num], [last_dot_num, -400]]
-
-
-
-i = 0
-
-while i<51 :
     color_names = ['red', 'orange', 'green', 'blue', 'purple', 'brown', 'pink', 'gray', 'black']
     random_color = random.choice(color_names)
     myPen.color(random_color)
 
-    if i<49 :
-        myPen.goto(final_list[i][0], final_list[i][1])
-        i += 1
-        continue
-    elif i == 49 :
-        last_dot = random.choice(final_dot_list)
-        myPen.goto(last_dot[0], last_dot[1])
-        i += 1
-        time.sleep(5)
-        break
+    dis = random.randrange(0,50)
+    degree_0 = random.randrange(0,360)
+    myPen.setheading(degree_0)
+    j = 0
+
+    if j == 0 :
+        myPen.left(degree_0)
+        myPen.forward(dis)
+        j = 1
+    
+    while j == 1 :
+        myPen.setheading(degree_0)
+        degree = random.randrange(degree_0-45,degree_0+45)
+        dis = random.randrange(dis,50)
+        myPen.left(degree)
+        myPen.forward(dis)
+        myPen.setheading(degree_0)
+        if abs(myPen.xcor()) >= 400 or abs(myPen.ycor()) >= 400 :
+            break
+turtle.mainloop()
+screen.tracer(1)
